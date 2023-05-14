56460163767977700f2da85b918eee92e7cc6cbf
lym12321 <i@coraa.cn>
2023-05-14 20:20:05 +0800

    📦 Update: Added some functions

--- dp2/data.go
+++ dp2/data.go
@@ -8,7 +8,7 @@ type LoginRequest struct {
 	StrParameters string `structs:"strParameters"` //'type=worker,client=REST|0.01'
 }
 
-type LoginResult struct {
+type LoginReturn struct {
 	LoginResult struct {
 		ErrorCode int    `json:"ErrorCode"`
 		ErrorInfo string `json:"ErrorInfo"`
@@ -19,7 +19,7 @@ type LoginResult struct {
 	StrLibraryCode    string `json:"strLibraryCode"`
 }
 
-type LogoutResult struct {
+type LogoutReturn struct {
 	LogoutResult struct {
 		ErrorCode int    `json:"ErrorCode"`
 		ErrorInfo string `json:"ErrorInfo"`
@@ -32,7 +32,7 @@ type GetReaderInfoRequest struct {
 	StrResultTypeList string `structs:"strResultTypeList"`
 }
 
-type GetReaderInfoResult struct {
+type GetReaderInfoReturn struct {
 	GetReaderInfoResult struct {
 		ErrorCode int    `json:"ErrorCode"`
 		ErrorInfo string `json:"ErrorInfo"`
@@ -47,9 +47,28 @@ type GetBiblioSummaryRequest struct {
 	StrItemBarcode string `structs:"strItemBarcode"`
 }
 
+type GetBiblioSummaryReturn struct {
+	GetBiblioSummaryResult struct {
+		ErrorCode int    `json:"ErrorCode"`
+		ErrorInfo string `json:"ErrorInfo"`
+		Value     int    `json:"Value"`
+	} `json:"GetBiblioSummaryResult"`
+	StrBiblioRecPath string `json:"strBiblioRecPath"`
+	StrSummary       string `json:"strSummary"`
+}
+
 type GetBiblioInfoRequest struct {
 	StrBiblioRecPath string `structs:"strBiblioRecPath"`
 	StrBiblioType    string `structs:"strBiblioType"`
+}
+
+type GetBiblioInfoReturn struct {
+	GetBiblioInfoResult struct {
+		ErrorCode int    `json:"ErrorCode"`
+		ErrorInfo string `json:"ErrorInfo"`
+		Value     int    `json:"Value"`
+	} `json:"GetBiblioInfoResult"`
+	StrBiblio string `json:"strBiblio"`
 }
 
 type BorrowRequest struct {
@@ -59,9 +78,60 @@ type BorrowRequest struct {
 	BForce           bool   `structs:"bForce"` // false
 }
 
+type BorrowReturn struct {
+	BorrowResult struct {
+		ErrorCode int    `json:"ErrorCode"`
+		ErrorInfo string `json:"ErrorInfo"`
+		Value     int    `json:"Value"`
+	} `json:"BorrowResult"`
+	ItemRecords   interface{} `json:"item_records"`
+	ReaderRecords interface{} `json:"reader_records"`
+	BiblioRecords interface{} `json:"biblio_records"`
+	BorrowInfo    struct {
+		BorrowCount      int         `json:"BorrowCount"`
+		BorrowID         string      `json:"BorrowID"`
+		BorrowOperator   string      `json:"BorrowOperator"`
+		DenyPeriod       string      `json:"DenyPeriod"`
+		ItemBarcode      string      `json:"ItemBarcode"`
+		LatestReturnTime string      `json:"LatestReturnTime"`
+		Overflows        interface{} `json:"Overflows"`
+		Period           string      `json:"Period"`
+	} `json:"borrow_info"`
+	ADupPath               interface{} `json:"aDupPath"`
+	StrOutputReaderBarcode string      `json:"strOutputReaderBarcode"`
+}
+
 type ReturnRequest struct {
 	StrAction      string `structs:"strAction"` // "return"
 	StrItemBarcode string `structs:"strItemBarcode"`
+}
+
+type ReturnReturn struct {
+	ReturnResult struct {
+		ErrorCode int    `json:"ErrorCode"`
+		ErrorInfo string `json:"ErrorInfo"`
+		Value     int    `json:"Value"`
+	} `json:"ReturnResult"`
+	ItemRecords            interface{} `json:"item_records"`
+	ReaderRecords          interface{} `json:"reader_records"`
+	BiblioRecords          interface{} `json:"biblio_records"`
+	ADupPath               interface{} `json:"aDupPath"`
+	StrOutputReaderBarcode string      `json:"strOutputReaderBarcode"`
+	ReturnInfo             struct {
+		BookType         string      `json:"BookType"`
+		BorrowCount      int         `json:"BorrowCount"`
+		BorrowID         string      `json:"BorrowID"`
+		BorrowOperator   string      `json:"BorrowOperator"`
+		BorrowTime       string      `json:"BorrowTime"`
+		Borrower         string      `json:"Borrower"`
+		ItemBarcode      string      `json:"ItemBarcode"`
+		LatestReturnTime string      `json:"LatestReturnTime"`
+		Location         string      `json:"Location"`
+		OverdueString    string      `json:"OverdueString"`
+		Period           string      `json:"Period"`
+		ReturnOperator   string      `json:"ReturnOperator"`
+		Volume           interface{} `json:"Volume"`
+	} `json:"return_info"`
 }
 
 type SearchReaderRequest struct {
@@ -73,6 +143,14 @@ type SearchReaderRequest struct {
 	StrResultSetName string `structs:"strResultSetName"` // default
 }
 
+type SearchReaderReturn struct {
+	SearchReaderResult struct {
+		ErrorCode int    `json:"ErrorCode"`
+		ErrorInfo string `json:"ErrorInfo"`
+		Value     int    `json:"Value"`
+	} `json:"SearchReaderResult"`
+}
+
 type SearchBiblioRequest struct {
 	StrReaderDbNames string `structs:"strReaderDbNames"` // "<all>"
 	StrQueryWord     string `structs:"strQueryWord"`
@@ -82,11 +160,40 @@ type SearchBiblioRequest struct {
 	StrResultSetName string `structs:"strResultSetName"` // default
 }
 
+type SearchBiblioReturn struct {
+	SearchBiblioResult struct {
+		ErrorCode int    `json:"ErrorCode"`
+		ErrorInfo string `json:"ErrorInfo"`
+		Value     int    `json:"Value"`
+	} `json:"SearchBiblioResult"`
+	StrQueryXML string `json:"strQueryXml"`
+}
+
 type GetSearchResultRequest struct {
 	StrResultSetName   string `structs:"strResultSetName"`
 	LStart             int    `structs:"lStart"`
 	LCount             int    `structs:"lCount"`
-	StrBorrowInfoStyle string `structs:"strBorrowInfoStyle"` // "id,cols"
+	StrBrowseInfoStyle string `structs:"strBrowseInfoStyle"` // "id,cols"
+}
+
+type GetSearchResultReturn struct {
+	GetSearchResultResult struct {
+		ErrorCode int    `json:"ErrorCode"`
+		ErrorInfo string `json:"ErrorInfo"`
+		Value     int    `json:"Value"`
+	} `json:"GetSearchResultResult"`
+	Searchresults []struct {
+		Cols       []string    `json:"Cols"`
+		Keys       interface{} `json:"Keys"`
+		Path       string      `json:"Path"`
+		RecordBody struct {
+			Metadata  string      `json:"Metadata"`
+			Path      string      `json:"Path"`
+			Result    interface{} `json:"Result"`
+			Timestamp interface{} `json:"Timestamp"`
+			XML       interface{} `json:"Xml"`
+		} `json:"RecordBody"`
+	} `json:"searchresults"`
 }
 
 type GetEntitiesRequest struct {
@@ -96,7 +203,41 @@ type GetEntitiesRequest struct {
 	StrStyle         string `structs:"strStyle"` // "onlygetpath"
 }
 
+type GetEntitiesReturn struct {
+	GetEntitiesResult struct {
+		ErrorCode int    `json:"ErrorCode"`
+		ErrorInfo string `json:"ErrorInfo"`
+		Value     int    `json:"Value"`
+	} `json:"GetEntitiesResult"`
+	Entityinfos []struct {
+		Action       string      `json:"Action"`
+		ErrorCode    int         `json:"ErrorCode"`
+		ErrorInfo    string      `json:"ErrorInfo"`
+		NewRecPath   string      `json:"NewRecPath"`
+		NewRecord    string      `json:"NewRecord"`
+		NewTimestamp interface{} `json:"NewTimestamp"`
+		OldRecPath   string      `json:"OldRecPath"`
+		OldRecord    string      `json:"OldRecord"`
+		OldTimestamp interface{} `json:"OldTimestamp"`
+		RefID        string      `json:"RefID"`
+		Style        string      `json:"Style"`
+	} `json:"entityinfos"`
+}
+
 type GetItemInfoRequest struct {
 	StrBarcode    string `structs:"strBarcode"`
 	StrResultType string `structs:"strResultType"`
+}
+
+type GetItemInfoReturn struct {
+	GetItemInfoResult struct {
+		ErrorCode int    `json:"ErrorCode"`
+		ErrorInfo string `json:"ErrorInfo"`
+		Value     int    `json:"Value"`
+	} `json:"GetItemInfoResult"`
+	StrResult        string `json:"strResult"`
+	StrItemRecPath   string `json:"strItemRecPath"`
+	ItemTimestamp    []int  `json:"item_timestamp"`
+	StrBiblio        string `json:"strBiblio"`
+	StrBiblioRecPath string `json:"strBiblioRecPath"`
 }
--- dp2/func.go
+++ dp2/func.go
@@ -10,16 +10,25 @@ import (
 )
 
 type SystemConfig struct {
-	ServerUrl   string
+	ServiceUrl  string
 	Dp2Username string
 	Dp2Password string
 	Session     *string
 }
 
 type Method interface {
-	Login() LoginResult
-	Logout() LogoutResult
-	GetReaderInfo(barcode string, retType string) GetReaderInfoResult
+	Login() LoginReturn
+	Logout() LogoutReturn
+	GetReaderInfo(barcode string, retType string) GetReaderInfoReturn
+	GetBiblioSummary(barcode string) GetBiblioSummaryReturn
+	GetBiblioInfo(recPath string, retType string) GetBiblioInfoReturn
+	Borrow(reader string, item string, renew bool) BorrowReturn
+	Return(item string) ReturnReturn
+	SearchReader(resultSetName string, keyword string) SearchReaderReturn
+	SearchBiblio(resultSetName string, keyword string) SearchBiblioReturn
+	GetSearchResult(resultSetName string, lStart int, lCount int) GetSearchResultReturn
+	GetEntities(recPath string) GetEntitiesReturn
+	GetItemInfo(barcode string, retType string) GetItemInfoReturn
 }
 
 func checkErr(err error) {
@@ -28,48 +37,207 @@ func checkErr(err error) {
 	}
 }
 
-func (s SystemConfig) Login() LoginResult {
+func (s SystemConfig) Login() LoginReturn {
 	// Data
 	raw := LoginRequest{StrUserName: s.Dp2Username, StrPassword: s.Dp2Password, StrParameters: "type=worker,client=REST|0.01"}
 	req := url.NewRequest()
 	req.Json = structs.Map(&raw)
 	// Post
-	ret, err := requests.Post(s.ServerUrl+"/login", req)
+	ret, err := requests.Post(s.ServiceUrl+"/login", req)
 	checkErr(err)
 	// Save this session
 	*s.Session = ret.Cookies[0].String()
 	// Return
-	var res LoginResult
+	var res LoginReturn
 	err = json.Unmarshal(ret.Content, &res)
 	checkErr(err)
 	return res
 }
 
-func (s SystemConfig) Logout() LogoutResult {
+func (s SystemConfig) Logout() LogoutReturn {
 	// Data
 	req := url.NewRequest()
-	req.Cookies = url.ParseCookies(s.ServerUrl, *s.Session)
+	req.Cookies = url.ParseCookies(s.ServiceUrl, *s.Session)
 	// Post
-	ret, err := requests.Post(s.ServerUrl+"/logout", req)
+	ret, err := requests.Post(s.ServiceUrl+"/logout", req)
 	checkErr(err)
 	// Return
-	var res LogoutResult
+	var res LogoutReturn
 	err = json.Unmarshal(ret.Content, &res)
 	checkErr(err)
 	return res
 }
 
-func (s SystemConfig) GetReaderInfo(barcode string, retType string) GetReaderInfoResult {
+func (s SystemConfig) GetReaderInfo(barcode string, retType string) GetReaderInfoReturn {
 	// Data
 	raw := GetReaderInfoRequest{StrBarcode: barcode, StrResultTypeList: retType}
 	req := url.NewRequest()
 	req.Json = structs.Map(&raw)
-	req.Cookies = url.ParseCookies(s.ServerUrl, *s.Session)
+	req.Cookies = url.ParseCookies(s.ServiceUrl, *s.Session)
 	// Post
-	ret, err := requests.Post(s.ServerUrl+"/getReaderInfo", req)
+	ret, err := requests.Post(s.ServiceUrl+"/getReaderInfo", req)
 	checkErr(err)
 	// Return
-	var res GetReaderInfoResult
+	var res GetReaderInfoReturn
+	err = json.Unmarshal(ret.Content, &res)
+	checkErr(err)
+	return res
+}
+
+func (s SystemConfig) GetBiblioSummary(barcode string) GetBiblioSummaryReturn {
+	// Data
+	raw := GetBiblioSummaryRequest{StrItemBarcode: barcode}
+	req := url.NewRequest()
+	req.Json = structs.Map(&raw)
+	req.Cookies = url.ParseCookies(s.ServiceUrl, *s.Session)
+	// Post
+	ret, err := requests.Post(s.ServiceUrl+"/getBiblioSummary", req)
+	checkErr(err)
+	// Return
+	var res GetBiblioSummaryReturn
+	err = json.Unmarshal(ret.Content, &res)
+	checkErr(err)
+	return res
+}
+
+func (s SystemConfig) GetBiblioInfo(recPath string, retType string) GetBiblioInfoReturn {
+	// Data
+	raw := GetBiblioInfoRequest{StrBiblioRecPath: recPath, StrBiblioType: retType}
+	req := url.NewRequest()
+	req.Json = structs.Map(&raw)
+	req.Cookies = url.ParseCookies(s.ServiceUrl, *s.Session)
+	// Post
+	ret, err := requests.Post(s.ServiceUrl+"/getBiblioInfo", req)
+	checkErr(err)
+	// Return
+	var res GetBiblioInfoReturn
+	err = json.Unmarshal(ret.Content, &res)
+	checkErr(err)
+	return res
+}
+
+func (s SystemConfig) Borrow(reader string, item string, renew bool) BorrowReturn {
+	// renew 为 true 时，执行续借操作
+	// Data
+	raw := BorrowRequest{StrReaderBarcode: reader, StrItemBarcode: item, BRenew: renew, BForce: false}
+	req := url.NewRequest()
+	req.Json = structs.Map(&raw)
+	req.Cookies = url.ParseCookies(s.ServiceUrl, *s.Session)
+	// Post
+	ret, err := requests.Post(s.ServiceUrl+"/Borrow", req)
+	checkErr(err)
+	// Return
+	var res BorrowReturn
+	err = json.Unmarshal(ret.Content, &res)
+	checkErr(err)
+	return res
+}
+
+func (s SystemConfig) Return(item string) ReturnReturn {
+	// Data
+	raw := ReturnRequest{StrItemBarcode: item, StrAction: "return"}
+	req := url.NewRequest()
+	req.Json = structs.Map(&raw)
+	req.Cookies = url.ParseCookies(s.ServiceUrl, *s.Session)
+	// Post
+	ret, err := requests.Post(s.ServiceUrl+"/Return", req)
+	checkErr(err)
+	// Return
+	var res ReturnReturn
+	err = json.Unmarshal(ret.Content, &res)
+	checkErr(err)
+	return res
+}
+
+func (s SystemConfig) SearchReader(resultSetName string, keyword string) SearchReaderReturn {
+	// 这里写死了查找方式 middle，以简化操作方式
+	// Data
+	raw := SearchReaderRequest{
+		StrQueryWord:     keyword,
+		StrReaderDbNames: "<all>",
+		NPerMax:          -1,
+		StrFrom:          "<all>",
+		StrMatchStyle:    "middle",
+		StrResultSetName: resultSetName,
+	}
+	req := url.NewRequest()
+	req.Json = structs.Map(&raw)
+	req.Cookies = url.ParseCookies(s.ServiceUrl, *s.Session)
+	// Post
+	ret, err := requests.Post(s.ServiceUrl+"/searchReader", req)
+	checkErr(err)
+	// Return
+	var res SearchReaderReturn
+	err = json.Unmarshal(ret.Content, &res)
+	checkErr(err)
+	return res
+}
+
+func (s SystemConfig) SearchBiblio(resultSetName string, keyword string) SearchBiblioReturn {
+	// 这里写死了查找方式 middle，以简化操作方式
+	// Data
+	raw := SearchBiblioRequest{
+		StrQueryWord:     keyword,
+		StrReaderDbNames: "<all>",
+		NPerMax:          -1,
+		StrFrom:          "<all>",
+		StrMatchStyle:    "middle",
+		StrResultSetName: resultSetName,
+	}
+	req := url.NewRequest()
+	req.Json = structs.Map(&raw)
+	req.Cookies = url.ParseCookies(s.ServiceUrl, *s.Session)
+	// Post
+	ret, err := requests.Post(s.ServiceUrl+"/searchBiblio", req)
+	checkErr(err)
+	// Return
+	var res SearchBiblioReturn
+	err = json.Unmarshal(ret.Content, &res)
+	checkErr(err)
+	return res
+}
+
+func (s SystemConfig) GetSearchResult(resultSetName string, lStart int, lCount int) GetSearchResultReturn {
+	raw := GetSearchResultRequest{StrResultSetName: resultSetName, LStart: lStart, LCount: lCount, StrBrowseInfoStyle: "id,cols"}
+	req := url.NewRequest()
+	req.Json = structs.Map(&raw)
+	req.Cookies = url.ParseCookies(s.ServiceUrl, *s.Session)
+	// Post
+	ret, err := requests.Post(s.ServiceUrl+"/getSearchResult", req)
+	checkErr(err)
+	// Return
+	var res GetSearchResultReturn
+	err = json.Unmarshal(ret.Content, &res)
+	checkErr(err)
+	return res
+}
+
+func (s SystemConfig) GetEntities(recPath string) GetEntitiesReturn {
+	// 考虑到同一路径下的书不会太多，这里默认返回全部信息
+	raw := GetEntitiesRequest{StrBiblioRecPath: recPath, LStart: 0, LCount: -1, StrStyle: "onlygetpath"}
+	req := url.NewRequest()
+	req.Json = structs.Map(&raw)
+	req.Cookies = url.ParseCookies(s.ServiceUrl, *s.Session)
+	// Post
+	ret, err := requests.Post(s.ServiceUrl+"/getEntities", req)
+	checkErr(err)
+	// Return
+	var res GetEntitiesReturn
+	err = json.Unmarshal(ret.Content, &res)
+	checkErr(err)
+	return res
+}
+
+func (s SystemConfig) GetItemInfo(barcode string, retType string) GetItemInfoReturn {
+	raw := GetItemInfoRequest{StrBarcode: barcode, StrResultType: retType}
+	req := url.NewRequest()
+	req.Json = structs.Map(&raw)
+	req.Cookies = url.ParseCookies(s.ServiceUrl, *s.Session)
+	// Post
+	ret, err := requests.Post(s.ServiceUrl+"/getItemInfo", req)
+	checkErr(err)
+	// Return
+	var res GetItemInfoReturn
 	err = json.Unmarshal(ret.Content, &res)
 	checkErr(err)
 	return res
